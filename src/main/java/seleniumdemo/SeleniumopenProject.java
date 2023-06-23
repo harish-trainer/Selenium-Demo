a6f1b4d7170ea96fb682f74405b51851cd3909c8
harishraj <harishbixtrainer@gmail.com>
2023-06-23 12:22:17 +0530

    inital changes in selenium demo

---
+++ src/main/java/seleniumdemo/SeleniumopenProject.java
@@ -0,0 +1,20 @@
+package seleniumdemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumopenProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriverManager.chromedriver().setup();
+
+WebDriver driver = new ChromeDriver();
+
+driver.get("https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager");
+	}
+
+}
